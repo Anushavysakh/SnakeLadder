b0fc28b047555e4935c09523ba97e80437df8f58
Anusha <anushavysakh232@gmail.com>
2022-07-17 19:30:54 +0530

    Committed uc2

--- game.java
+++ game.java
@@ -1,14 +1,11 @@
 package com.Assignment4.bridlabz;
 
 //import java.util.Random;
-
 public class SnakeLadder {
 
-	public static void main(String args[]) {
-		int initialPo=0;
-		System.out.println("Welcome too Snake and Ladder Game");
-		System.out.println("Player's Initial Position is: " + initialPos);
+	public static void main(String[] args) {
+		int diceRoll = 1 + (int)( (Math.random() *10) % 6);
+		System.out.println("The Dice Rolled to: " + diceRoll);
 	}
 }
-		
 	
