e9ad535c1f83c73004b0ef16aa1b1cb3a6e5a4e7
Anusha <anushavysakh232@gmail.com>
2022-07-17 19:25:52 +0530

    Snake and ladder

--- game.java
+++ game.java
@@ -5,7 +5,10 @@ package com.Assignment4.bridlabz;
 public class SnakeLadder {
 
 	public static void main(String args[]) {
+		int initialPo=0;
 		System.out.println("Welcome too Snake and Ladder Game");
-		
+		System.out.println("Player's Initial Position is: " + initialPos);
 	}
 }
+		
+	
