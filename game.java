040e8d472a4bc4023a8bda054a26de1524ac41d9
Anusha <anushavysakh232@gmail.com>
2022-07-17 19:09:51 +0530

    Snake and ladder

---
+++ game.java
@@ -0,0 +1,11 @@
+package com.Assignment4.bridlabz;
+
+//import java.util.Random;
+
+public class SnakeLadder {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome too Snake and Ladder Game");
+		
+	}
+}
